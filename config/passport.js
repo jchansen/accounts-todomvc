514a1eb6ce0b19866515ea6cb4e9a690e6e659b7
jchansen <jason_hansen@outlook.com>
2014-04-12 14:35:59 -0700

    renamed stormpath config to passport for clarity

--- config/passport.js
+++ config/passport.js
@@ -1,44 +1,36 @@
-//var passport = require('passport'),
-//    LocalStrategy = require('passport-local').Strategy,
-//    bcrypt = require('bcrypt');
-//
-//passport.serializeUser(function (user, done) {
-//  done(null, user.id);
-//});
-//
-//passport.deserializeUser(function (id, done) {
-//  User.findOneById(id, function (err, user) {
-//    done(err, user);
-//  });
-//});
-//
-//passport.use(new LocalStrategy(
-//    function (username, password, done) {
-//      User.findOneByUsername(username).done(function (err, user) {
-//        if (err) {
-//          return done(null, err);
-//        }
-//        if (!user) {
-//          return done(null, false, { message: 'No account under that username'});
-//        }
-//        bcrypt.compare(password, user.password, function (err, res) {
-//          if (!res) return done(null, false, { message: 'Invalid password'});
-//          return done(null, user);
-//        });
-//      });
-//    })
-//);
-//
-//module.exports = {
-//  express: {
-//    customMiddleware: function (app) {
-//      console.log('express midleware for passport');
-//      app.use(passport.initialize());
-//      app.use(passport.session());
-//      app.use(function(req, res, next){
-//        res.locals.user = req.user;
-//        next();
-//      });
-//    }
-//  }
-//};
+var passport = require('passport'),
+  StormpathStrategy = require('../lib/passport-stormpath').Strategy;
+
+passport.serializeUser(function (user, done) {
+  // serialize the entire object into the session
+  // todo: serialize into an in-memory store or redis
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  // since the user is already in the session just pass it through
+  done(null, user);
+});
+
+passport.use(new StormpathStrategy({
+    applicationURL: 'https://api.stormpath.com/v1/applications/5onePoMSC0FZAGyd48xrOJ',
+    apiKeyId: process.env.STORMPATH_API_KEY_ID,
+    apiKeySecret: process.env.STORMPATH_API_KEY_SECRET
+  })
+);
+
+module.exports = {
+  express: {
+    customMiddleware: function (app) {
+      console.log('express midleware for passport');
+      app.use(passport.initialize());
+      app.use(passport.session({
+        domain:'.bonsaidigital.co'
+      }));
+      app.use(function(req, res, next){
+        res.locals.user = req.user;
+        next();
+      });
+    }
+  }
+};
